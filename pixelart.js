b8ff03198561eb1456d9381465c5db4256b6d766
oaksvirals <oaksvirals@gmail.com>
2023-10-30 17:45:59 -0500

    Working resize grid
    Slider now replaces old one with a new one.

--- pixelart.js
+++ pixelart.js
@@ -13,8 +13,6 @@
 
 // Setup for grabbing initial containers, creating pixel box container, and applying css
 const outerContainer = document.querySelector('.centerContainer');
-const pixelContainer = document.createElement('div');
-pixelContainer.className = 'pixelContainer';
 
 // Pixel Box
 let rowSize = 16;
@@ -25,49 +23,62 @@ let pixelColor = 'black';
 
 const createPixels = (num) => {
 
-    function creationEvent() {
-        // start fresh for any new container
-        pixelContainer.remove();
-        outerContainer.appendChild(pixelContainer);
+    const pixelContainer = document.createElement('div');
+    pixelContainer.className = 'pixelContainer';
 
-        // adjust container size if boxes do not fit
-        if (containerSize % rowSize > 0) {
-            let sizing = containerSize / rowSize;
-            let adjustment = Math.round(sizing);
-
-            containerSize = rowSize * adjustment;
-
-            pixelBoxSizing = containerSize + 'px';
-
-            pixelContainer.style.maxWidth = pixelBoxSizing;
-            pixelContainer.style.maxHeight = pixelBoxSizing;
-        };
-
-        // create inner boxes "pixels"
-        pixelWidthHeight = containerSize / rowSize + 'px';
-
+    // start fresh for any new container
+    outerContainer.appendChild(pixelContainer);
+    
+    // adjust container size if boxes do not fit
+    if (containerSize % rowSize > 0) {
+        let sizing = containerSize / rowSize;
+        let adjustment = Math.round(sizing);
+    
+        containerSize = rowSize * adjustment;
+    
+        pixelBoxSizing = containerSize + 'px';
+    
+        pixelContainer.style.maxWidth = pixelBoxSizing;
+        pixelContainer.style.maxHeight = pixelBoxSizing;
+    };
+    
+    // create inner boxes "pixels"
+    pixelWidthHeight = containerSize / rowSize + 'px';
+    
         for (let i = 0; i < num; i++) {
-            const pixelBox = document.createElement('div');
-
-            pixelContainer.appendChild(pixelBox);
-            pixelBox.style.width = pixelWidthHeight;
-            pixelBox.style.height = pixelWidthHeight;
-            pixelBox.className = 'pixelBox';
-            pixelBox.backgroundColor = 'white';
-
-            // hover effects
+        const pixelBox = document.createElement('div');
+    
+        pixelContainer.appendChild(pixelBox);
+        pixelBox.style.width = pixelWidthHeight;
+        pixelBox.style.height = pixelWidthHeight;
+        pixelBox.className = 'pixelBox';
+        pixelBox.backgroundColor = 'white';
+    
+        // hover effects
             pixelBox.addEventListener('mouseenter', () => {
                 pixelBox.style.backgroundColor = pixelColor;
             });
         };
-    };
-
-    creationEvent()
 };
 
 // create initial box at beginning
 createPixels(rowSize * rowSize);
 
+// slider
+const slider = document.querySelector('.slider');
+const rangeValue = document.querySelector('#rangeValue');
+
+slider.addEventListener('input', () => {
+    rangeValue.textContent = slider.value;
+});
+
+slider.addEventListener('mouseup', () => {
+    rowSize = slider.value;
+    outerContainer.innerHTML = '';
+    createPixels(rowSize * rowSize);
+})
+
+// menu
 // dropper color picker
 const dropper = document.querySelector('.dropper');
 const colorPicker = document.querySelector('#colorPicker');
